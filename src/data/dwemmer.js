af39183ac830fe3793d218e0d20fd00b93369f67
viktorHek <viktorkarlsson125@gmail.com>
2024-04-08 11:14:37 +0200

    dwemer lore

--- src/data/dwemmer.js
+++ src/data/dwemmer.js
@@ -1,14 +1,73 @@
 const dwemmer = {
   title: "Elder Scroll Lore Theory",
+  size: "big",
   html: (
     <div className="content">
-      <h3>What Happend to the Dwemer?</h3>
+      <h2>What Happend to the Dwemer?</h2>
+      <br />
       <h4>Backstory</h4>
+      <br />
       <p>
         The dwemer, or deep-elves, was a remnants of the early aldmer. They are well known for their
         skill and revolutionary developments in technology, engineering, crafting methods,
         metalwork, stonework, architecture, city-planning, science, mathematics, magic, and the
         academic arts.
+      </p>
+      <p>
+        One of the greates dwemer to ever live was Chief Tonal Architect Kagrenac. Tonal
+        Architecture is unique to the dwemer people. Many people know that the world is the result
+        of a conversation between the two gods Anu and Padomay. But the tonal architectures are the
+        only ones that knows how to manipulate the voises of the gods to reconstuct reality.
+      </p>
+      <p>
+        The dwemer where never fond of the gods and the ide of being destroyed if Anus and Padomays
+        conversation reached it's conclution sure must have scared them. When the dwemers found the
+        heart of Lorkan, the dead god, Lord Kagrenac started working on the biggest technological
+        advansment in the history of Mundus. With the power to manipulate the voice of gods, Lord
+        Kagremac built a one thousand feet tall automaton that would use the heart of Lorkan as it's power source.
+        Lord Kagrenac built Numidium, the brass God of destruction. But when the Numidium was
+        activated, a dragonend started and all the dwemer disapperad. Everyone exept one, Yagrum
+        Bagarn, he was in oblivion at the time (this will de relevant leater). This occured
+        thousands of years ago and no one have heard anything about was heppend to the dwemer.
+      </p>
+
+      <br />
+      <h4>What happend?</h4>
+      <br />
+      <p>
+        There are two common theories and I think both are wrong. the first one is called zero-sum.
+        The theory is that by the power of the Numidium and trough global communication system, all
+        dwemer reached was is called chim and reliced that they are just a dream in the god-head.
+        When reaching chim there are two outcomes. You relice thet you don't actually exist and
+        disappear or you relice thet you don't actually need to exist and you become a god. I don't
+        like this theory because the dwemer already knew that they where the result of a
+        conversation. The relication that they don't really exist should not be that surpricing.
+      </p>
+      <p>
+        The second theory is that the dwemer became "the skin of the Numidium". Lord Kagrenac was
+        very patriotic and would never to something to destroy his people. And he's to smart to not
+        see this as a posible side effect.
+      </p>
+      <p>
+        The two theorys above are based on the ide that the disappearance of the dwemer was a
+        failure. My theory is that the dwemer succeeded.
+      </p>
+      <p>
+        The whole point of creating the Numidium was in order to not be depended on the gods. Both
+        Mundus and oblivion will cease to exist if the conversation between Anu and Padomay stops. I
+        believe Lord Kagrenac reliced that the only way to get away from the gods was to travle
+        outside of both mundus and oblivion, to "The Gray Maybe". The Numidium was never intended to
+        destroy material things (even though it could, see Tiber Septims conquest over the Aldmeri
+        Dominion of the Summerset Isles.) The Numidium was intended to destroy the walls beween
+        mundus and "The Gray Maybe".
+      </p>
+      <p>
+        The reason why we have never heard of an dwemer is because they live in an dimention that no
+        one else has visitet. The reason why Yagrum Bagarn is still here is because the Numidium
+        created an opening between mundus and "The Gray Maybe" witch was not conected to oblivion.
+        This theory also explains how Tiber Septim manage to defeat the military superior Aldmeri
+        Dominion in less then an hour. He didn't (really) defeat them, he teleported them to "The
+        Gray Maybe".
       </p>
     </div>
   ),
